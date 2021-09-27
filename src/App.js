58d982f101623a81653e629eefb4a0794175e802
Prithiviraj Murugan (RBEI/ECD42-EC) <Murugan.Prithiviraj@in.bosch.com>
2021-09-27 09:38:46 +0530

    finished app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>
+{
+  const [isAdd, setIsAdd] = useState(false);
+  const [todoName, setTodoName] = useState('');
+  const [todoMsg, setTodoMsg] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const todoItem = { id: uuid(), todoName, todoMsg };
+    setTodoList([...todoList, todoItem]);
+    
+  }
+
+  const removeHandler = (id) => {
+    const newTodoList = todoList.filter((todoItem) => { return todoItem.id != id });
+    setTodoList(newTodoList);
+  }
+
+  return <>
+    <section>
+      <label htmlFor="isAdd">Add TodoItem?</label>
+      <input type="checkbox" id="isAdd" name="isAdd" value={isAdd}
+        onChange={() => {
+          setIsAdd(!isAdd);
+        }} />
+      {isAdd ?(<form onSubmit={(e)=>submitHandler(e)}>
+        <label htmlFor="todoName">What to do?</label>
+        <input type="text" name="todoName" id="todoName"
+          value={todoName} onChange={(e) => { setTodoName(e.target.value);}}/>
+        <label htmlFor="todoMsg">Message</label>
+        <input type="text" name="todoMsg" id="todoMsg"
+          value={todoMsg} onChange={(e) => { setTodoMsg(e.target.value); }}/>
+        <button type="submit">Add TODO</button>
+      </form>) : null}
+      
+    {todoList.map((todoItem) => {
+          const { id, todoName, todoMsg } = todoItem;
+          return (
+            <div key={id}>
+              <h4>{todoName}</h4>
+              <p>{todoMsg}</p>
+              <button onClick={() => { removeHandler(id) }}>remove</button>
+            </div>
+          );
+    })}
+      <button onClick={() => { setTodoList([])} }>Remove All</button>
+        </section>
+  </>;
 }
 
 export default App;
